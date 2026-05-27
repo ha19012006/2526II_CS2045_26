192680675b03e61ddab66cfad6b494a4dca48c9e
ha19012006 <52032612+ha19012006@users.noreply.github.com>
2026-05-27 16:49:44 +0700

    Delete customer.py

--- customer.py
+++
@@ -1,6 +0,0 @@
-class Customer:
-    def __init__(self, age, income, credit_score, employment):
-        self.age = age
-        self.income = round(income, 1)
-        self.credit_score = credit_score
-        self.employment = employment
